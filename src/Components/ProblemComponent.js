b2674b65bf19283cc470bac63e78aedfa61f6bf9
Nick Perry <nicholasjperry1@gmail.com>
2021-02-09 13:45:00 -0600

    Nick's third commit

---
+++ src/Components/ProblemComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+class Problem extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            solution: false
+        };
+    }
+}
+
+export default Problem;
---
+++ src/shared/Problems.js
@@ -0,0 +1,26 @@
+export const Problems = [
+    {
+        id: 0,
+        name:'Problem 1',
+        completed: false,
+        difficulty: "Easy"
+    },
+    {
+        id: 1,
+        name:'Problem 2',
+        completed: false,
+        difficulty: "Easy"
+    },
+    {
+        id: 2,
+        name:'Problem 3',
+        completed: false,
+        difficulty: "Easy",
+    },
+    {
+        id: 3,
+        name:'Problem 4',
+        completed: false,
+        difficulty: "Medium",
+    },
+];
