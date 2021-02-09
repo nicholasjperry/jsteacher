0c52669c96e600dbb4cc7169101ee02b3581aae7
Turn of Phrase <wllptrsn1992@gmail.com>
2021-02-09 01:24:14 -0600

    Third Commit: A little bit of Functionality.

--- src/Components/ProblemsComponent.js
+++ src/Components/ProblemsComponent.js
@@ -1,23 +1,92 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, Button } from 'reactstrap';
+import TextEditor from './TextEditorComponent';
+import { Tasks } from '../shared/Problems';
 //import {Redirect } from 'react-router-dom';
 
 class Problems extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentTask: 0,
+            tasks: Tasks
+
+        };
+    }
+
+    showProblems() {
+        document.querySelector('.problems-Landing').style.display = "none";
+        document.querySelector('.problemsChoice').style.display = "inline-block";
+
+    }
+
+    specificTask(x) {
+        document.querySelector('.problemsChoice').style.display = "none";
+        document.querySelector('.playground').style.display = "inline-block";
+        this.setState({
+            currentTask: x
+        });
+    }
+    
+
+    quitBtn() {
+        document.querySelector('.problems-Landing').style.display = "inline-block";
+        document.querySelector('.playground').style.display = "none";
+    }
+    
+    doneBtn() {
+        alert('Clicking on this Button will check the code');
+    }
     render() {
+        const EachProblem = this.state.tasks.map(task => {
+            return (
+                <tr>
+                    <td  className= "problemTitle" onClick={() => this.specificTask(task.id)}>
+                        {task.name}
+                    </td>
+
+                    <td>
+                        {task.difficulty}
+                    </td>
+
+                    <td>
+                        <img width={100} src = {task.completed} alt ="false" />
+                    </td>
+                </tr>
+            );
+        });
+
         return (
             <React.Fragment>
                 <Container className="full-page">
                     <Row>
-                        <Col>
+                        <Col className="problems-Landing">
                             <h2 className="page-Heading">
                                 Learn about the javascript fundamentals via the problems below
                             </h2>
 
-                            <Button>Try the App</Button>
+                            <Button onClick={this.showProblems}>Try the App</Button>
                         </Col>
-                    </Row>
-                    <Row>
 
+
+                        <Col xs={12} sm={12} className="problemsChoice">
+                            <table >
+                                <tr>
+                                    <th>Title</th>
+                                    <th>Difficulty</th>
+                                    <th>Completed</th>
+                                </tr>
+                                {EachProblem}
+                            </table>
+                        </Col>
+
+
+                        <Col xs={12} sm={12} className="playground">
+                            <Col><strong>Objective</strong>:{this.state.tasks[this.state.currentTask].directions}</Col>
+                            <Col><TextEditor /></Col>
+                            <Col><Button onClick={this.quitBtn}>Quit</Button><Button onClick={this.doneBtn}>Done</Button></Col>
+                        </Col>
                     </Row>
                 </Container>
             </React.Fragment>
---
+++ src/shared/Problems.js
@@ -0,0 +1,30 @@
+export const Tasks = [
+    {
+        id: 0,
+        name:'Variable Assignment',
+        completed: '/assets/false.png',
+        difficulty: "Easy",
+        directions: "Assign two different numbers (1 & 2) to two different variables and add them together."
+    },
+    {
+        id: 1,
+        name:'Data Types - Arrays',
+        completed: '/assets/false.png',
+        difficulty: "Easy",
+        directions: "Find what is at position 2 within the array \"Michael, Nick, Will\".."
+    },
+    {
+        id: 2,
+        name:'Data Types - Arrays',
+        completed: '/assets/false.png',
+        difficulty: "Easy",
+        directions: "Let your array be equal to \"keyboard, mouse, monitor, speakers\".  Remove \"speakers\" from the array and add \"headphones\"."
+    },
+    {
+        id: 3,
+        name:'Conditionals',
+        completed: '/assets/false.png',
+        difficulty: "Medium",
+        directions: "Log to console the statement \"It\'s going to freeze!\" if the temperature is less than or equal to 32.  Otherwise, let it say, \"It\'s still a liquid.\""
+    },
+];
